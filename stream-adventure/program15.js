30dd2dbbd497c2bb1f514d5cc6620b19c9ffd32e
Michael <mzc2fd@virginia.edu>
2014-12-25 16:43:57 -0500

    updates on some more fiddling

--- stream-adventure/program15.js
+++ stream-adventure/program15.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
 
-console.log("beep boop")
+var crypto = require('crypto')
+var tar = require('tar')
+var zlib = require('zlib')
+
+var stream = crypto.createDecipher(process.argv[2], process.argv[3])
+var parser = tar.Parse()
+
+parser.on('entry', function(entry) {
+	if(entry.type != 'File')
+		return
+
+	var md5Hash = crypto.createHash('md5', {encoding: 'hex' })
+	entry.on('data', function(data) {
+		md5Hash.update(data)
+	})
+
+	entry.on('end', function() {
+		var digest = md5Hash.digest('hex')
+		console.log(digest + ' ' + entry.path)
+	})
+})
+
+process.stdin
+	.pipe(stream)
+	.pipe(zlib.createGunzip())
+	.pipe(parser)
